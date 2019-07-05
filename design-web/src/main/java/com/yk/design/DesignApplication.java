dcb2a296b8926c01558a7fcf5e1f419d4dad0079
kang.yang <Kang.Yang@b-and-qchina.com>
2019-07-05 09:11:27 +0800

    首页

--- design-web/src/main/java/com/yk/design/DesignApplication.java
+++ design-web/src/main/java/com/yk/design/DesignApplication.java
@@ -2,9 +2,15 @@ package com.yk.design;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DesignApplication {
+public class DesignApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DesignApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DesignApplication.class, args);
